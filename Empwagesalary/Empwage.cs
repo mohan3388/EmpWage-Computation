7e32aa36bb255169abc24d014c7867048691e6e1
mohan3388 <68227271+mohan3388@users.noreply.github.com>
2022-04-19 17:08:57 +0530

    Update Empwage.cs

--- Empwagesalary/Empwage.cs
+++ Empwagesalary/Empwage.cs
@@ -6,31 +6,29 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageComputation
 {
-    public class Employeesalary
+    public class EmployeeWage
     {
-        
         const int PartTime = 1;
         const int FullTime = 2;
         const int EmpWagePrHr = 20;
         const int FullDay_Working_Hr = 8;
         const int PartTime_Time_Working_Hr = 4;
-        const int EmpWorking_Pr_Month= 20;
+        const int EmpWorking_Pr_Month = 20;
         const int Max_Working_Hr = 100;
-         
-       
-        
+
+
         int empHrs = 0;
-        int EmpTotalSalary=0;
+        int EmpTotalSalary = 0;
         int EmpTotalEmpHrs = 0;
         int day = 0;
-      
-        public void monthlyWage()
+
+        public void monthlyEmpWage()
         {
             while (day <= EmpWorking_Pr_Month && empHrs <= Max_Working_Hr)
             {
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
-                
+
                 switch (empCheck)
                 {
                     case FullTime:
@@ -46,13 +44,12 @@ namespace EmployeeWageComputation
                         break;
                 }
                 day++;
-
             }
 
             EmpTotalSalary = empHrs * EmpWagePrHr;
             Console.WriteLine("One month Employee Salary is :" + EmpTotalSalary);
         }
-       
+
 
     }
 }
