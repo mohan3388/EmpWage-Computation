3aa013b9e6cafa0a78a300edab65d357e67ddf8b
mohan338 <sahumohan338@gmail.com>
2022-04-19 17:43:51 +0530

    [mohan]add:emp wage

--- Empwage/Empwage.cs
+++ Empwage/Empwage.cs
@@ -4,19 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Empwage
+namespace EmployeeWageComputation
 {
     public class EmployeeWage
     {
-        const int IS_PRESENT = 1;
-        public void empAttandence()
+      
+        const int PartTime = 1;
+        const int FullTime = 2;
+        const int EmpFullDay_WorkHr = 8;
+        const int EmpPartTime_WorkHr = 4;
+
+        
+        int empHrs = 0, totalEmpSalary = 0, day = 0;
+
+        public void monthlyEmpWage(string Company_Name, int EMP_WAGE_PR_HR, int MAX_WORKING_HR, int EMP_WORKING_Days_PR_MONTH)
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == 1)
-                Console.WriteLine("Employee is Present");
-            else
-                Console.WriteLine("Employee is Absent");
+            while (day <= EMP_WORKING_Days_PR_MONTH && empHrs <= MAX_WORKING_HR)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case FullTime:
+
+                        empHrs += EmpFullDay_WorkHr;
+                        break;
+                    case PartTime:
+
+                        empHrs += EmpPartTime_WorkHr;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                day++;
+
+            }
+
+            totalEmpSalary = empHrs * EMP_WAGE_PR_HR;
+            Console.WriteLine("One month Employee Salary is :" + totalEmpSalary);
         }
+
+
     }
 }
--- Empwage/Program.cs
+++ Empwage/Program.cs
@@ -1,3 +1,18 @@
-﻿using Empwage;
-EmployeeWage attandence = new EmployeeWage();
-attandence.empAttandence();
+﻿using System;
+using EmployeeWageComputation;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        EmployeeWage computation = new EmployeeWage();
+        Console.WriteLine("Enter the Compnay Name");
+        string Comp_Name = Console.ReadLine();
+        Console.WriteLine("Enter the Employee Salary Pr Hours");
+        int empWage_PrHours = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Maximum Working Hours Of That Company");
+        int maxWorking_Hour = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Working Days in One Month");
+        int workingDays_In_OneMonth = Convert.ToInt32(Console.ReadLine());
+        computation.monthlyEmpWage(Comp_Name, empWage_PrHours, maxWorking_Hour, workingDays_In_OneMonth);
+    }
+}
